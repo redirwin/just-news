0081ffc181acc3d73242dc645f448c236674d0af
Dave Irwin <david@redirwin.com>
2019-07-25 12:53:28 -0700

    Cloned to local, created react app, set up for project.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" />
+    </div>
+  );
+}
+
+export default App;
